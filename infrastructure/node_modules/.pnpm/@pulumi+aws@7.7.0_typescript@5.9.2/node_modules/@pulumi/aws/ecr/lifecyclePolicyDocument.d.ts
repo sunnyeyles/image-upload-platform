cd2abeb1b9906e78bfb37f02b70456bc90ac0f1f
sunny <sunnyeyles@gmail.com>
2025-09-29 18:18:08 +1000

    Removed pulumi

--- infrastructure/node_modules/.pnpm/@pulumi+aws@7.7.0_typescript@5.9.2/node_modules/@pulumi/aws/ecr/lifecyclePolicyDocument.d.ts
+++
@@ -1,5 +0,0 @@
-import * as inputs from '../types/input';
-export type LifecyclePolicyDocument = inputs.ecr.LifecyclePolicyDocument;
-export type PolicyRule = inputs.ecr.LifecyclePolicyRule;
-export type Action = inputs.ecr.LifecyclePolicyAction;
-export type Selection = inputs.ecr.LifecyclePolicySelection;
